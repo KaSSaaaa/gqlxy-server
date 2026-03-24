2ec1a5399a4706618049961597e2c112439c0668
kassa_ <pierre.jacobs852@gmail.com>
2026-03-24 17:56:38 +0100

    Fix CI

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -1,5 +1,9 @@
 find_program(GCOVR_EXECUTABLE gcovr REQUIRED)
 
+get_filename_component(_compiler_name "${CMAKE_CXX_COMPILER}" NAME)
+string(REGEX REPLACE "^g\\+\\+(-[0-9.]+)?$" "gcov\\1" _gcov_name "${_compiler_name}")
+find_program(GCOV_EXECUTABLE NAMES "${_gcov_name}" gcov REQUIRED)
+
 set(COVERAGE_HTML_DIR ${CMAKE_SOURCE_DIR}/coverage)
 
 add_custom_target(coverage
@@ -9,6 +13,7 @@ add_custom_target(coverage
         --output-on-failure
     COMMAND ${CMAKE_COMMAND} -E make_directory ${COVERAGE_HTML_DIR}
     COMMAND ${GCOVR_EXECUTABLE}
+        --gcov-executable ${GCOV_EXECUTABLE}
         --root ${CMAKE_SOURCE_DIR}
         --filter "${CMAKE_SOURCE_DIR}/src/"
         --filter "${CMAKE_SOURCE_DIR}/include/"
